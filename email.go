5eb6b4fe4be34ba8e02b6a4310cd21729edc475c
Cassondra Foesch <puellanivis@gmail.com>
2019-09-23 21:12:42 +0000

    fix some utf8 details

--- email.go
+++ email.go
@@ -6,14 +6,14 @@ import (
 
 const (
 	emailFWS           = `[ ]?`
-	emailQtext         = emailFWS + `[^\t\n\r "\\` + utf8ExcludeInvalid + `]+`
-	emailQuotedPair    = emailFWS + `\\[^\n\r` + utf8ExcludeInvalid + `]`
+	emailQtext         = emailFWS + `[^\t\n\r "\\` + utf8Invalid + `]+`
+	emailQuotedPair    = emailFWS + `\\[^\n\r` + utf8Invalid + `]`
 	emailQcontent      = `(?:` + emailQtext + `|` + emailQuotedPair + `)`
 	emailQuotedString  = `"` + emailQcontent + `*` + emailFWS + `"`
 	emailAtext         = `[!#\$%&'\*\+\-/0-9=\?A-Z\^_\x60a-z{|}~` + utf8NonASCII + `]+`
 	emailDotAtom       = `(?:` + emailAtext + `(?:\.` + emailAtext + `)*)`
 	emailLocalPart     = `(?:` + emailDotAtom + `|` + emailQuotedString + `)`
-	emailDtext         = emailFWS + `[^\t\n\r \[\\\]]+`
+	emailDtext         = emailFWS + `[^\t\n\r \[\\\]` + utf8Invalid + `]+`
 	emailDcontent      = `(?:` + emailDtext + `|` + emailQuotedPair + `)`
 	emailDomainLiteral = `\[` + emailDcontent + `*` + emailFWS + `\]`
 	emailDomain        = `(?:` + hostnameString + `|` + emailDomainLiteral + `)`

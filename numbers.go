3f6a8e1e43d42a4d2663f0435093adc7b667eaf7
Cassondra Foesch <puellanivis@gmail.com>
2019-09-24 11:58:08 +0000

    expand numbers

--- numbers.go
+++ numbers.go
@@ -1,7 +1,18 @@
 package pedantic
 
 const (
-	digitHex      = `[0-9A-Fa-f]`
+	digitBin = `[01]`
+	digitOct = `[0-7]`
+	digitDec = asciiNumeric
+	digitHex = `[` + digitDec + `A-Fa-f]`
+
+	numberByteBin = `(?:0|1[01]{0,7})`
+	numberByteOct = `(?:0|[1-3](?:[0-7][0-7]?)?|[4-7][0-7]?)`
 	numberByteDec = `(?:0|1(?:[0-9][0-9]?)?|2(?:[0-4][0-9]?|5[0-5]?|[6-9])?|[3-9][0-9]?)`
-	numberByteHex = `(?:` + digitHex + digitHex + `)`
+	numberByteHex = `(?:0|[1-9A-Fa-f][0-9A-Fa-f]?)`
+
+	numberByteBinFull = `(?:` + digitBin + digitBin + digitBin + digitBin + digitBin + digitBin + digitBin + digitBin + `)`
+	numberByteOctFull = `(?:[0-3][0-7][0-7])`
+	numberByteDecFull = `(?:[01][0-9][0-9]|2[0-4][0-9]|25[0-5])`
+	numberByteHexFull = `(?:` + digitHex + digitHex + `)`
 )
--- numbers_test.go
+++ numbers_test.go
@@ -7,7 +7,117 @@ import (
 	"testing"
 )
 
-const numberByteDecRegexString = `^` + numberByteDec + `$`
+const (
+	numberByteBinRegexString = `^` + numberByteBin + `$`
+	numberByteOctRegexString = `^` + numberByteOct + `$`
+	numberByteDecRegexString = `^` + numberByteDec + `$`
+	numberByteHexRegexString = `^` + numberByteHex + `$`
+)
+
+const (
+	numberByteBinFullRegexString = `^` + numberByteBinFull + `$`
+	numberByteOctFullRegexString = `^` + numberByteOctFull + `$`
+	numberByteDecFullRegexString = `^` + numberByteDecFull + `$`
+	numberByteHexFullRegexString = `^` + numberByteHexFull + `$`
+)
+
+func TestByteBin(t *testing.T) {
+	r, err := syntax.Parse(numberByteBinRegexString, syntax.Perl)
+	if err != nil {
+		t.Fatal("unexpected error ", err)
+	}
+	t.Log(r.Simplify())
+
+	type test struct {
+		s     string
+		match bool
+	}
+
+	var tests []test
+	for i := 0; i < 256; i++ {
+		tests = append(tests, test{fmt.Sprintf("%b", i), true})
+	}
+	tests = append(tests, test{"100000000", false})
+	for i := 0; i < 128; i++ {
+		tests = append(tests, test{fmt.Sprintf("0%b", i), false})
+	}
+	for i := 0; i < 64; i++ {
+		tests = append(tests, test{fmt.Sprintf("00%b", i), false})
+	}
+	for i := 0; i < 32; i++ {
+		tests = append(tests, test{fmt.Sprintf("000%b", i), false})
+	}
+	for i := 0; i < 16; i++ {
+		tests = append(tests, test{fmt.Sprintf("0000%b", i), false})
+	}
+	for i := 0; i < 8; i++ {
+		tests = append(tests, test{fmt.Sprintf("00000%b", i), false})
+	}
+	for i := 0; i < 4; i++ {
+		tests = append(tests, test{fmt.Sprintf("000000%b", i), false})
+	}
+	for i := 0; i < 2; i++ {
+		tests = append(tests, test{fmt.Sprintf("0000000%b", i), false})
+	}
+	tests = append(tests, test{"00000000", false})
+	tests = append(tests, test{"-1", false})
+	tests = append(tests, test{"alpha", false})
+
+	NumberByteBin := regexp.MustCompile(numberByteBinRegexString)
+
+	for _, tt := range tests {
+		got := NumberByteBin.MatchString(tt.s)
+		if got != tt.match {
+			switch tt.match {
+			case true:
+				t.Errorf("expected %q to match, but it did not", tt.s)
+			case false:
+				t.Errorf("expected %q to not match, but it did", tt.s)
+			}
+		}
+	}
+}
+
+func TestByteOct(t *testing.T) {
+	r, err := syntax.Parse(numberByteOctRegexString, syntax.Perl)
+	if err != nil {
+		t.Fatal("unexpected error ", err)
+	}
+	t.Log(r.Simplify())
+
+	type test struct {
+		s     string
+		match bool
+	}
+
+	var tests []test
+	for i := 0; i < 256; i++ {
+		tests = append(tests, test{fmt.Sprintf("%o", i), true})
+	}
+	tests = append(tests, test{"400", false})
+	for i := 0; i < 0100; i++ {
+		tests = append(tests, test{fmt.Sprintf("0%o", i), false})
+	}
+	for i := 0; i < 010; i++ {
+		tests = append(tests, test{fmt.Sprintf("00%o", i), false})
+	}
+	tests = append(tests, test{"-1", false})
+	tests = append(tests, test{"alpha", false})
+
+	NumberByteOct := regexp.MustCompile(numberByteOctRegexString)
+
+	for _, tt := range tests {
+		got := NumberByteOct.MatchString(tt.s)
+		if got != tt.match {
+			switch tt.match {
+			case true:
+				t.Errorf("expected %q to match, but it did not", tt.s)
+			case false:
+				t.Errorf("expected %q to not match, but it did", tt.s)
+			}
+		}
+	}
+}
 
 func TestByteDec(t *testing.T) {
 	r, err := syntax.Parse(numberByteDecRegexString, syntax.Perl)
@@ -39,6 +149,225 @@ func TestByteDec(t *testing.T) {
 
 	for _, tt := range tests {
 		got := NumberByteDec.MatchString(tt.s)
+		if got != tt.match {
+			switch tt.match {
+			case true:
+				t.Errorf("expected %q to match, but it did not", tt.s)
+			case false:
+				t.Errorf("expected %q to not match, but it did", tt.s)
+			}
+		}
+	}
+}
+
+func TestByteHex(t *testing.T) {
+	r, err := syntax.Parse(numberByteHexRegexString, syntax.Perl)
+	if err != nil {
+		t.Fatal("unexpected error ", err)
+	}
+	t.Log(r.Simplify())
+
+	type test struct {
+		s     string
+		match bool
+	}
+
+	var tests []test
+	for i := 0; i < 256; i++ {
+		tests = append(tests, test{fmt.Sprintf("%x", i), true})
+		tests = append(tests, test{fmt.Sprintf("%X", i), true})
+	}
+	tests = append(tests, test{"100", false})
+	for i := 0; i < 0x10; i++ {
+		tests = append(tests, test{fmt.Sprintf("0%x", i), false})
+		tests = append(tests, test{fmt.Sprintf("0%X", i), false})
+	}
+	tests = append(tests, test{"-1", false})
+	tests = append(tests, test{"alpha", false})
+
+	NumberByteHex := regexp.MustCompile(numberByteHexRegexString)
+
+	for _, tt := range tests {
+		got := NumberByteHex.MatchString(tt.s)
+		if got != tt.match {
+			switch tt.match {
+			case true:
+				t.Errorf("expected %q to match, but it did not", tt.s)
+			case false:
+				t.Errorf("expected %q to not match, but it did", tt.s)
+			}
+		}
+	}
+}
+
+func TestByteBinFull(t *testing.T) {
+	r, err := syntax.Parse(numberByteBinFullRegexString, syntax.Perl)
+	if err != nil {
+		t.Fatal("unexpected error ", err)
+	}
+	t.Log(r.Simplify())
+
+	type test struct {
+		s     string
+		match bool
+	}
+
+	var tests []test
+	for i := 0; i < 256; i++ {
+		tests = append(tests, test{fmt.Sprintf("%08b", i), true})
+	}
+	tests = append(tests, test{"100000000", false})
+	for i := 0; i < 128; i++ {
+		tests = append(tests, test{fmt.Sprintf("%b", i), false})
+	}
+	for i := 0; i < 64; i++ {
+		tests = append(tests, test{fmt.Sprintf("0%b", i), false})
+	}
+	for i := 0; i < 32; i++ {
+		tests = append(tests, test{fmt.Sprintf("00%b", i), false})
+	}
+	for i := 0; i < 16; i++ {
+		tests = append(tests, test{fmt.Sprintf("000%b", i), false})
+	}
+	for i := 0; i < 8; i++ {
+		tests = append(tests, test{fmt.Sprintf("0000%b", i), false})
+	}
+	for i := 0; i < 4; i++ {
+		tests = append(tests, test{fmt.Sprintf("00000%b", i), false})
+	}
+	for i := 0; i < 2; i++ {
+		tests = append(tests, test{fmt.Sprintf("000000%b", i), false})
+	}
+	tests = append(tests, test{"0", false})
+	tests = append(tests, test{"-1", false})
+	tests = append(tests, test{"alpha", false})
+
+	NumberByteBinFull := regexp.MustCompile(numberByteBinFullRegexString)
+
+	for _, tt := range tests {
+		got := NumberByteBinFull.MatchString(tt.s)
+		if got != tt.match {
+			switch tt.match {
+			case true:
+				t.Errorf("expected %q to match, but it did not", tt.s)
+			case false:
+				t.Errorf("expected %q to not match, but it did", tt.s)
+			}
+		}
+	}
+}
+
+func TestByteOctFull(t *testing.T) {
+	r, err := syntax.Parse(numberByteOctFullRegexString, syntax.Perl)
+	if err != nil {
+		t.Fatal("unexpected error ", err)
+	}
+	t.Log(r.Simplify())
+
+	type test struct {
+		s     string
+		match bool
+	}
+
+	var tests []test
+	for i := 0; i < 256; i++ {
+		tests = append(tests, test{fmt.Sprintf("%03o", i), true})
+	}
+	tests = append(tests, test{"400", false})
+	for i := 0; i < 0100; i++ {
+		tests = append(tests, test{fmt.Sprintf("%o", i), false})
+	}
+	for i := 0; i < 010; i++ {
+		tests = append(tests, test{fmt.Sprintf("0%o", i), false})
+	}
+	tests = append(tests, test{"-1", false})
+	tests = append(tests, test{"alpha", false})
+
+	NumberByteOctFull := regexp.MustCompile(numberByteOctFullRegexString)
+
+	for _, tt := range tests {
+		got := NumberByteOctFull.MatchString(tt.s)
+		if got != tt.match {
+			switch tt.match {
+			case true:
+				t.Errorf("expected %q to match, but it did not", tt.s)
+			case false:
+				t.Errorf("expected %q to not match, but it did", tt.s)
+			}
+		}
+	}
+}
+
+func TestByteDecFull(t *testing.T) {
+	r, err := syntax.Parse(numberByteDecFullRegexString, syntax.Perl)
+	if err != nil {
+		t.Fatal("unexpected error ", err)
+	}
+	t.Log(r.Simplify())
+
+	type test struct {
+		s     string
+		match bool
+	}
+
+	var tests []test
+	for i := 0; i < 256; i++ {
+		tests = append(tests, test{fmt.Sprintf("%03d", i), true})
+	}
+	tests = append(tests, test{"256", false})
+	for i := 0; i < 100; i++ {
+		tests = append(tests, test{fmt.Sprintf("%d", i), false})
+	}
+	for i := 0; i < 10; i++ {
+		tests = append(tests, test{fmt.Sprintf("0%d", i), false})
+	}
+	tests = append(tests, test{"-1", false})
+	tests = append(tests, test{"alpha", false})
+
+	NumberByteDecFull := regexp.MustCompile(numberByteDecFullRegexString)
+
+	for _, tt := range tests {
+		got := NumberByteDecFull.MatchString(tt.s)
+		if got != tt.match {
+			switch tt.match {
+			case true:
+				t.Errorf("expected %q to match, but it did not", tt.s)
+			case false:
+				t.Errorf("expected %q to not match, but it did", tt.s)
+			}
+		}
+	}
+}
+
+func TestByteHexFull(t *testing.T) {
+	r, err := syntax.Parse(numberByteHexFullRegexString, syntax.Perl)
+	if err != nil {
+		t.Fatal("unexpected error ", err)
+	}
+	t.Log(r.Simplify())
+
+	type test struct {
+		s     string
+		match bool
+	}
+
+	var tests []test
+	for i := 0; i < 256; i++ {
+		tests = append(tests, test{fmt.Sprintf("%02x", i), true})
+		tests = append(tests, test{fmt.Sprintf("%02X", i), true})
+	}
+	tests = append(tests, test{"100", false})
+	for i := 0; i < 0x10; i++ {
+		tests = append(tests, test{fmt.Sprintf("%x", i), false})
+		tests = append(tests, test{fmt.Sprintf("%X", i), false})
+	}
+	tests = append(tests, test{"-1", false})
+	tests = append(tests, test{"alpha", false})
+
+	NumberByteHexFull := regexp.MustCompile(numberByteHexFullRegexString)
+
+	for _, tt := range tests {
+		got := NumberByteHexFull.MatchString(tt.s)
 		if got != tt.match {
 			switch tt.match {
 			case true:
